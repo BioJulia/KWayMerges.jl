34c807e8b3917c86207af8e4b34d3ab00c40d4c2
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2025-06-30 19:22:56 +0200

    Change API

--- src/KWayMerges.jl
+++ src/KWayMerges.jl
@@ -5,28 +5,33 @@ export KWayMerger
 include("heap.jl")
 
 """
-    KWayMerger{F, T, I, S}
+    KWayMerger{T, I, F}(f::F, iterators)
+    KWayMerger{T, I}(iterators)
+    KWayMerger(f, iterators)
+    KWayMerger(iterators)
 
-Stateful iterator which does a K-way merge between multiple
+Create a stateful iterator which does a k-way merge between multiple
 iterators of the same type.
-This iterator will yield the elements in every contained
-iterator. At each iteration, it will choose the element from
-the iterator with the lowest precedence according to the order
-determined by `f::F` (default: `isless`).
 
-If all inner iterators are sorted by `f`, the yielded elements
-will be in sorted order.
-A `KWayMerger` is typically used to combined multiple sorted arrays
+This iterator yields `(index::Int, x::T)` elements, where `x` is the next element from
+one of the iterators, and `index` is the 1-based index of the iterator that yielded `x`.
+The elements `x` are chosen from among the iterators such that, among all elements which
+are the next element of the iterators, the element is chosen which is the smallest
+according to the predicate `f::F`, which defaults to `isless`.
+
+This implies that if all iterators are sorted by `f`, the yielded will be in sorted
+order.
+Hence, a `KWayMerger` is typically used to combined multiple sorted arrays
 into one sorted array.
 
 # Examples
 ```jldoctest
-julia> arrs = [[1,4,10], [2,3], [6,8], [5,7,9]];
+julia> arrs = [[1,6], [2], [5,7], [3,4,8]];
 
 julia> it = KWayMerger(arrs);
 
 julia> print(collect(it))
-[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+[(1, 1), (2, 2), (4, 3), (4, 4), (3, 5), (1, 6), (3, 7), (4, 8)]
 ```
 
 # Extended help
@@ -37,69 +42,51 @@ The type parameters are:
 * `I`: Iterator type
 * `S`: Type of state of iterators
 
-All iterators must be of the same type. `Base.eltype` is used
-to determine `T` and `I`; since its default implementation
-returns `Any`, these type parameters might need to be explicitly
-passed to get good performance for some iterators.
+All iterators must be of the same type. For the constructors which don't pass
+in `T` and `I` explicitly, `Base.eltype` is used
+to determine them; since its default implementation
+returns `Any`, explicitly passing them may be needed for good performance for some
+iterators.
 
 `S` is derived automatically, but this must be a fixed type;
 iterators that use states of multiple different types may
 not be supported by `KWayMerger`.
 """
-struct KWayMerger{F, T, I, S}
+struct KWayMerger{T, I, F, S}
     f::F
-    iterators::Memory{I}
-    states::Memory{S}
-    heap::Vector{Tuple{T, Int}}
+    iterators::Vector{I}
+    states::Vector{S}
+    heap::Vector{Tuple{Int, T}}
 end
 
 ord(x::KWayMerger) = ord(x.f)
-ord(f) = (i, j) -> f(first(i), first(j))
+ord(f) = (i, j) -> f(last(i), last(j))
 
-collect_to_memory(x) = collect_to_memory(Base.IteratorSize(typeof(x)), x)
-
-function collect_to_memory(::Union{Base.HasShape, Base.HasLength}, x)
-    mem = Memory{eltype(x)}(undef, length(x))
-    i = 0
-    for it in x
-        i += 1
-        mem[i] = it
-    end
-    i == length(mem) || error("Implementation error: More elements than reported")
-    return mem
-end
-
-function collect_to_memory(::Base.SizeUnknown, x)
-    T = eltype(typeof(x))
-    v = collect(T, x)
-    return copy!(Memory{T}(undef, length(v)), v)
-end
-
-function KWayMerger{F, T, I}(f::F, iterators) where {F, T, I}
-    iters = collect_to_memory(iterators)
+function KWayMerger{T, I, F}(f::F, iterators) where {T, I, F}
+    iters = vec(collect(iterators))
     states = nothing
-    things = Tuple{T, Int}[]
+    things = Tuple{Int, T}[]
     for i in eachindex(iters)
         it = iterate(iters[i])
         isnothing(it) && continue
         (thing::T, state) = it
         if isnothing(states)
-            states = Memory{typeof(state)}(undef, length(iters))
+            states = Vector{typeof(state)}(undef, length(iters))
         end
-        push!(things, (thing, i))
+        push!(things, (i, thing))
         states[i] = state
     end
     heapify!(ord(f), things)
     states = if isnothing(states)
-        Memory{Union{}}(undef, length(iters))
+        Vector{Union{}}(undef, length(iters))
     else
         states
     end
-    return KWayMerger{F, T, I, eltype(states)}(f, iters, states, things)
+    return KWayMerger{T, I, F, eltype(states)}(f, iters, states, things)
 end
 
 function KWayMerger{T, I}(iterators) where {T, I}
-    return KWayMerger{typeof(isless), T, I}(isless, iterators)
+    return KWayMerger{T, I, typeof(isless)}(isless, iterators)
 end
 
 KWayMerger(iterators) = KWayMerger(isless, iterators)
@@ -107,17 +94,17 @@ KWayMerger(iterators) = KWayMerger(isless, iterators)
 function KWayMerger(f::F, iterators) where {F}
     I = eltype(typeof(iterators))
     T = eltype(I)
-    return KWayMerger{F, T, I}(f, iterators)
+    return KWayMerger{T, I, F}(f, iterators)
 end
 
 # We could technically know this, but KWayMerger is stateful, and
 # Julia's iterator length works badly with stateful iterators.
 Base.IteratorSize(::Type{<:KWayMerger}) = Base.SizeUnknown()
-Base.eltype(::Type{<:KWayMerger{F, T}}) where {F, T} = T
+Base.eltype(::Type{<:KWayMerger{T}}) where {T} = Tuple{Int, T}
 
 function Base.iterate(x::KWayMerger, ::Nothing = nothing)
     isempty(x.heap) && return nothing
-    (item, i) = @inbounds x.heap[1]
+    (i, item) = @inbounds x.heap[1]
     iterator = @inbounds x.iterators[i]
     state = @inbounds x.states[i]
     it = iterate(iterator, state)
@@ -126,16 +113,16 @@ function Base.iterate(x::KWayMerger, ::Nothing = nothing)
     else
         (new_item, new_state) = it
         @inbounds x.states[i] = new_state
-        @inbounds heapreplace!(ord(x.f), x.heap, (new_item, i))
+        @inbounds heapreplace!(ord(x.f), x.heap, (i, new_item))
     end
-    return (item, nothing)
+    return ((i, item), nothing)
 end
 
 Base.isempty(x::KWayMerger) = isempty(x.heap)
 Base.isdone(x::KWayMerger) = isempty(x.heap)
 
 """
-    peek(x::KWayMerger)::Union{Some, Nothing}
+    peek(x::KWayMerger{T})::Union{Tuple{Int, T}, Nothing}
 
 Get the first element of `x` without advancing the iterator, or `nothing` if the
 iterator is empty.
@@ -145,17 +132,17 @@ iterator is empty.
 julia> it = KWayMerger([[3, 4], [2, 7]]);
 
 julia> peek(it)
-Some(2)
+(2, 2)
 
-julia> collect(it); # empty stateful iterator
+julia> collect(it); # exhaust stateful iterator
 
 julia> peek(it) === nothing
 true
 ```
 """
-function Base.peek(x::KWayMerger{F, T})::Union{Some{T}, Nothing} where {F, T}
+function Base.peek(x::KWayMerger)
     isempty(x.heap) && return nothing
-    return @inbounds Some(x.heap[1][1])
+    return @inbounds x.heap[1]
 end
 
 end # module KWayMerges
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,48 +6,55 @@ using Test
 @testset "Construction" begin
     v = [1:3, 4:6, 9:11]
 
-    T = KWayMerger{typeof(isless), Int, UnitRange{Int}, Int}
+    T = KWayMerger{Int, UnitRange{Int}, typeof(isless), Int}
 
-    @test KWayMerger{typeof(isless), Int, UnitRange{Int}}(isless, v) isa T
+    @test KWayMerger{Int, UnitRange{Int}, typeof(isless)}(isless, v) isa T
     @test KWayMerger{Int, UnitRange{Int}}(v) isa T
     @test KWayMerger(v) isa T
     @test KWayMerger(isless, v) isa T
-    @test KWayMerger(<, v) isa KWayMerger{typeof(<), Int, UnitRange{Int}}
+    @test KWayMerger(<, v) isa KWayMerger{Int, UnitRange{Int}, typeof(<)}
+end
+
+function manual_collect(it; lt=isless, by=identity)
+    v = Iterators.map(enumerate(it)) do (i, inner_it)
+        zip(Iterators.repeated(i), inner_it)
+    end |> Iterators.flatten |> collect
+    sort!(v; by = i -> by(last(i)), lt)
 end
 
 @testset "Forward sorting" begin
     v = [[3, 5, 8], [1, 1], [10, 11], [1, 2, 7], Int[]]
-    r = sort(collect(Iterators.flatten(v)))
 
-    @test collect(KWayMerger(v)) == r
+    @test collect(KWayMerger(v)) == manual_collect(v)
 end
 
 @testset "Using a predicate" begin
     v = [["de", "abc"], [""], ["xysa", "dsakljdwe"]]
-    r = sort(collect(Iterators.flatten(v)); by = length)
+
+    r = manual_collect(v; by=length)
 
     @test collect(KWayMerger((i, j) -> isless(length(i), length(j)), v)) == r
 end
 
 @testset "Reverse sorting" begin
-    v = [[3, 5, 8], [1, 1], [10, 11], [1, 2, 7], Int[]]
+    v = [[3, 5, 8], [1, 0], [10, 11], [4, 2, 7], Int[]]
     for i in v
         sort!(i; rev = true)
     end
-    r = sort(collect(Iterators.flatten(v)); rev = true)
+    r = manual_collect(v; lt = >)
 
     @test collect(KWayMerger((i, j) -> isless(j, i), v)) == r
 end
 
 @testset "Some edge cases" begin
     it = KWayMerger([])
-    @test it isa KWayMerger{typeof(isless)}
+    @test it isa (KWayMerger{T, I, typeof(isless)} where {T, I})
     @test collect(it) == Any[]
 
     it = KWayMerger([1:0, 11:10])
-    @test it isa KWayMerger{typeof(isless), Int}
-    @test collect(it) == Int[]
-    @test typeof(collect(it)) == Vector{Int}
+    @test it isa KWayMerger{Int, UnitRange{Int}, typeof(isless)}
+    @test collect(it) == Tuple{Int, Int}[]
+    @test typeof(collect(it)) == Vector{Tuple{Int, Int}}
 end
 
 end # module KWayMergesTest
